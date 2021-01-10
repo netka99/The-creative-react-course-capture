a8b529d44373b012e62fa5c2b4c0b40eea609663
Aneta Di Scala <38839156+netka99@users.noreply.github.com>
2021-01-10 21:25:11 +0100

    Our Work updated with movie list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React from "react";
+//Global Style
+import GlobalStyle from "./components/GlobalStyle";
+//import pages
 import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
+//Router
+import { Switch, Route } from "react-router-dom";
+import MovieDetail from "./pages/MovieDetail";
 
 function App() {
   return (
     <div className="App">
-      <AboutUs />
+      <GlobalStyle />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work" exact>
+          <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
